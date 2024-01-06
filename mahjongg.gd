e7f841df64d023c3730df64e464541baf8b7854f
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-01-06 10:47:57 -0600

    basically a stash, but includes a titlescreen now and some other goodies. unsure if in a working state.

--- mahjongg.gd
+++ mahjongg.gd
@@ -5,9 +5,13 @@ func spawn_tiles():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	SignalBus.new_game.connect(_on_new_game)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_new_game():
+	print("[mahjongg] Starting a new game...")
+	remove_child($TitleScreen)
+	spawn_tiles()
--- signal_bus.gd
+++ signal_bus.gd
@@ -3,3 +3,5 @@ extends Node
 signal tile_added_to_hand(tile: Area2D, new_resting_point: Vector2)
 signal hand_reorder_tiles(tile: Area2D, new_resting_point: Vector2) # TODO combine these?
 signal tile_removed_from_hand(tile: Area2D)
+
+signal new_game
