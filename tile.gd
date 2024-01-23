058e1b6ef982dce91bfc29ad8cb5d8798fafed53
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-01-23 01:01:02 -0600

    Reworked tile perspective-spritesheet selection logic. TODO BUG one tile in the test is invisible when face up. Is this a bug in the tile spawn (wrong index? no problem if so) or a perspective logic bug (bad)?

--- tile.gd
+++ tile.gd
@@ -4,6 +4,8 @@ extends Sprite2D
 var suit: String
 var value: int
 var faceup: bool
+# The "orientation" of the tile (see perspective dicts)
+var perspective = "bottom"
 
 # Interaction and location parameters
 var selected = false
@@ -12,45 +14,63 @@ var rest_point: Vector2  # This is provided by a hand.
 var frozen = false
 #var rest_nodes = []
 
+const suits = ["crak", "boo", "dot", "honor"] # TODO BUG TERRIBLE! THIS IS NOT SHARED WITH ONE IN mahjongg.gd!!!
 
-func freeze():
-	frozen = true
-
-
-func unfreeze():
-	frozen = false
-
+func set_perspective(persp: String):
+	perspective = persp
 
 func turn_face_down():
-	frame_coords = Vector2(0, 2)
+	# TODO add to setter
 	faceup = false
+	const face_down_offset = 2
+	var perspective_offset = _get_perspective_offset()
+	frame_coords = Vector2(perspective_offset, face_down_offset)
+
+
+func _get_suit_offset() -> int:
+	return suits.find(suit)
 
 
 func turn_face_up():
-	# Get the right value based on what was set before calling add_child()
-	const suits = ["crak", "boo", "dot", "honor"] # TODO BUG TERRIBLE! THIS IS NOT SHARED WITH ONE IN mahjongg.gd!!!
-	const first_suit_offset = 3
-	frame_coords = Vector2(value, first_suit_offset + suits.find(suit))
 	faceup = true
+	# The face up tiles start at row index 3.
+	const face_up_initial_offset = 3
+	# Additional offset based off the perspective (which side of the tile can you see?)
+	var perspective_offset = _get_perspective_offset()
+	# Additional offset (from crak (crak == O regradless of perspective)) for the suit (see suit enum for indices)
+	var suit_offset = _get_suit_offset()
+	
+	var y = face_up_initial_offset + perspective_offset + suit_offset
+	frame_coords = Vector2(value - 1, y)  # Convert value to index
+	
 
-
-func change_perspective(perspective: String):
-	# There are y values
-	const sprite_perspective_offsets_faceup = {"bottom": 1, "right": 3, "top": 0, "left": 2}
-	# These are x values
-	const sprite_perspective_offsets_facedown = {"bottom": 0, "right": 6, "top": 2, "left": 6}
-	if perspective not in sprite_perspective_offsets_facedown and perspective not in sprite_perspective_offsets_faceup:
-		print("ERROR")
-		get_tree().quit(1)
-		
+func _get_perspective_offset() -> int:
+	# Returns the offset, in rows (x vals) from the very first face up tile
+	# that matches the given tile set. OR, if face-down, returns the row
 	if faceup:
-		frame_coords.y = 3 + sprite_perspective_offsets_faceup[perspective]
+		# These are rows in the spritesheet, which are "y" values.
+		# These are the crak rows
+		const sprite_perspective_offsets_faceup = {"bottom": 5, "top": 0, "left": 10, "right": 10}
+		return sprite_perspective_offsets_faceup[perspective] 
 	else:
-		frame_coords.x = sprite_perspective_offsets_facedown[perspective]
-	
-	
+		# All the face down sprites are in a single row, so these are x values
+		const sprite_perspective_offsets_facedown = {"bottom": 0, "top": 2, "left": 6, "right": 6}
+		return sprite_perspective_offsets_facedown[perspective]
+
+
+func _validate_fields():
+	# TODO use setters for this instead for extra validation!
+	# Value is not an index. We convert it to one when appropriate internally.
+	assert(value >= 1 and value <= 9)
+	assert(suit in suits)
+	assert(Vector2.ZERO <= rest_point and rest_point <= Vector2(1600,900))
+	#assert(perspective in ["bottom", "top", "left", "right"])
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_validate_fields()
+	
 	SignalBus.tile_added_to_hand.connect(_on_hand_collect_tile_into_hand)
 	SignalBus.tile_removed_from_hand.connect(_on_hand_remove_tile_from_hand)
 	SignalBus.hand_reorder_tiles.connect(_on_hand_reorder_tiles)
@@ -61,11 +81,13 @@ func _ready():
 #	rest_point = rest_nodes[0].global_position  # Default resting position (may not be necessary)
 #	rest_nodes[0].select()  # Update color indicators
 
+
 func _on_control_gui_input(_event):
 	if Input.is_action_just_pressed("click") and not frozen:  # TODO does one of the nodes already have a frozen flag?
 		selected = true
 		move_to_front()
 		turn_face_up()
+
 
 func _physics_process(delta):
 	if selected:
@@ -76,6 +98,7 @@ func _physics_process(delta):
 	else:
 		global_position = lerp(global_position, rest_point, 10 * delta)
 #		rotation = lerp_angle(rotation, 0, 10 * delta)
+
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -108,6 +131,7 @@ func _on_hand_remove_tile_from_hand(tile: Area2D):
 
 	print("[Tile] out of hand")
 	in_hand = false
+
 
 func _on_hand_reorder_tiles(area: Area2D, new_rest_point: Vector2):
 	if area != $Area2D:
---
+++ tile_tester.gd
@@ -0,0 +1,81 @@
+extends Node2D
+
+# Straight from mahjongg.gd
+func spawn_tiles():
+	var tile_scene = preload("res://tile.tscn")
+	const suits = ["crak", "boo", "dot", "honor"]  # TODO BUG TERRIBLE! THIS IS NOT SHARED WITH ONE IN mahjongg.gd!!!
+	for suit in suits.slice(0, 3):
+		var tile = tile_scene.instantiate()
+		tile.suit = suit
+		tile.value = 1
+		#tile.position = Vector2(300 + 50 * (value + i), 200 + 100 * suits.find(suit))
+		tile.position = Vector2(randi_range(100, 1500), randi_range(100, 700))
+		tile.rest_point = tile.position
+		tile.frozen = true
+		tile.add_to_group("tiles")
+		add_child(tile)
+	
+	# Winds and Dragons
+	# TODO these don't really have a "value", but it's convenient to match the normal tiles for now
+	for value in range(1, 7+1):
+		var tile = tile_scene.instantiate()
+		tile.suit = "honor"
+		tile.value = value
+		tile.position = Vector2(randi_range(100, 1500), randi_range(100, 700))
+		tile.rest_point = tile.position
+		tile.frozen = true
+		tile.add_to_group("tiles")
+		add_child(tile)
+	# Flowers and Jokers
+	for i in 1:
+		var tile = tile_scene.instantiate()
+		# TODO we don't have textures for these yet, so just use the ? tile for now I guess
+		tile.suit = "honor"
+		tile.value = 8
+		tile.position = Vector2(randi_range(100, 1500), randi_range(100, 700))
+		tile.rest_point = tile.position
+		tile.frozen = true
+		tile.add_to_group("tiles")
+		add_child(tile)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	test_tile_perspective()
+
+
+func test_tile_perspective():
+	spawn_tiles()
+	
+	get_tree().call_group("tiles", "turn_face_up")
+	$StepLabel.text = "up"
+	await get_tree().create_timer(1).timeout
+	for persp in ["bottom", "top", "left", "right"]:
+		get_tree().call_group("tiles", "set_perspective", persp)
+		get_tree().call_group("tiles", "turn_face_up")
+		$StepLabel.text = "up " + persp
+		await get_tree().create_timer(1).timeout 
+	
+	get_tree().call_group("tiles", "turn_face_down")
+	$StepLabel.text = "down"
+	await get_tree().create_timer(1).timeout
+	for persp in ["bottom", "top", "left", "right"]:
+		get_tree().call_group("tiles", "set_perspective", persp)
+		get_tree().call_group("tiles", "turn_face_down")
+		$StepLabel.text = "down " + persp
+		await get_tree().create_timer(1).timeout 
+	
+	$StepLabel.text = "repetition test..."
+	await get_tree().create_timer(1).timeout 
+	
+	# Test back and forth
+	for persp in ["bottom", "top", "left", "right"]:
+		get_tree().call_group("tiles", "set_perspective", persp)
+		for i in 2:
+			get_tree().call_group("tiles", "turn_face_up")
+			$StepLabel.text = "up " + persp
+			await get_tree().create_timer(1).timeout 
+			get_tree().call_group("tiles", "turn_face_down")
+			$StepLabel.text = "down " + persp
+			await get_tree().create_timer(1).timeout 
+	
+	get_tree().quit()
