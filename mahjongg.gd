0f3933f8f6dc26c1157404e82de67ebfd6a1f2a1
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-06-25 18:23:12 -0400

    Both horiz walls built. Is there enough vertical space??

--- mahjongg.gd
+++ mahjongg.gd
@@ -27,7 +27,7 @@ func spawn_tiles():
 			tile.position = Vector2(300 + 50 * (value + i), 200 + 100 * tile.suit)
 			#tile.position = Vector2(randi_range(100, 1500), randi_range(100, 700))
 			tile.rest_point = tile.position
-			tile.frozen = false
+			tile.frozen = true
 			tile.add_to_group("tiles")
 			add_child(tile)
 	
@@ -40,14 +40,21 @@ func spawn_tiles():
 		tile.position = Vector2(300 + 50 * (i + 1), 200 + 100 * (tile.suit + 1))
 		#tile.position = Vector2(randi_range(100, 1500), randi_range(100, 700))
 		tile.rest_point = tile.position
-		tile.frozen = false
+		tile.frozen = true
 		tile.add_to_group("tiles")
 		add_child(tile)
 
-func build_horizontal_wall(wall_length: int, tiles: Array, x: int, bottom_tile_y: int, second_level_offset: int, x_y_offset: Vector2):
-	# WARNING: This consumes the tiles array! Send a copy to it. TODO just reference it
+func build_horizontal_wall(
+	wall_length: int, 
+	tiles: Array, 
+	x: int, 
+	bottom_tile_y: int, 
+	second_level_offset: int, 
+	x_y_offset: Vector2
+):
+	# WARNING: This consumes the tiles array! Send a copy to it. TODO just reference it.
 	# Two levels tall. First one has no offset because it's the "origin".
-	for level_offset in [Vector2(0, 0), Vector2(0, second_level_offset)]:
+	for level_offset in [Vector2.ZERO, Vector2(0, second_level_offset)]:
 		for i in wall_length:
 			# Move each tile in the array to its spot on the wall.
 			# TODO make a wall building animation that is representative of real life
@@ -58,19 +65,9 @@ func build_horizontal_wall(wall_length: int, tiles: Array, x: int, bottom_tile_y
 			if level_offset:
 				tile.move_to_front()
 			
-			tile.frozen = false
+			tile.frozen = true
 			#tile.change_perspective("bottom")
-			await get_tree().create_timer(0.2).timeout # waits for 1 second
-	
-	# Do it again, but slightly offset to show two levels
-	#for i in wall_length:
-		#var tile = tiles.pop_front()
-		#tile.move_to_front()
-		#tile.rest_point = wall_first_spot_upper + tile_offset * i + horiz_wall_offset * n
-		#tile.frozen = false
-		#tile.change_perspective(perspective)
-		#await get_tree().create_timer(0.03).timeout # waits for 1 second
-	
+			await get_tree().create_timer(0.1).timeout # waits for 1 second
 	
 func build_wall():
 	const wall_length = 19
@@ -80,7 +77,10 @@ func build_wall():
 	
 	tiles.shuffle()
 	
+	# Right now we're hard-coded to 1600x900.
+	const x_offset_from_bottom = null
 	build_horizontal_wall(wall_length, tiles, 300, 750, -20, Vector2(52, 0))
+	build_horizontal_wall(wall_length, tiles, 300, 150, -20, Vector2(52, 0))
 	# TODO is tiles consumed at this point?
 
 
