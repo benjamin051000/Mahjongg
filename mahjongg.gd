1ca567d4b7af8bdb9e81fe590ea6882a01ec7043
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-12-15 13:39:58 -0500

    3 walls are properly indexed.
    The fourth wall is broken! lol
    
    Something broke and it doesn't display the layers anymore. TODO fix

--- mahjongg.gd
+++ mahjongg.gd
@@ -53,14 +53,23 @@ func build_horizontal_wall(
 	x: int, # Starting x-coord of wall (NOTE which appears to be the center of the tile)
 	bottom_tile_y: int, # Y-coord of the bottom row
 	second_level_offset: int, # y-offset of top level
-	x_y_offset: Vector2i  # Size of tile, used as offset. TODO can we implicitly get this from the Tile size?
-):
+	x_y_offset: Vector2i,  # Size of tile, used as offset. TODO can we implicitly get this from the Tile size?
+	bottom_not_top: bool, # true == bottom, false == top
+) -> Array[Node]:
+	var tiles_in_this_wall: Array[Node] = []
+
 	# WARNING: This consumes the tiles array! TODO send a copy, or just reference it.
 	# Two levels tall. First one has no offset because it's the "origin".
+	var level_offsets = [Vector2i.ZERO, Vector2i(0, second_level_offset)]
+	if not bottom_not_top:
+		level_offsets.reverse()
+		
 	for i in wall_length:
-		for level_offset in [Vector2i.ZERO, Vector2i(0, second_level_offset)]:
+		for level_offset in level_offsets:
 			# Move each tile in the array to its spot on the wall.
 			var tile = tiles.pop_front()
+			tiles_in_this_wall.append(tile)
+
 			tile.rest_point = Vector2i(x, bottom_tile_y) + x_y_offset*i + level_offset
 			
 			# Ensure the top level appears on top of the bottom level.
@@ -68,7 +77,13 @@ func build_horizontal_wall(
 				tile.move_to_front()
 			
 			#tile.frozen = false
-			await get_tree().create_timer(0.05).timeout
+			await get_tree().create_timer(0.01).timeout
+
+	if bottom_not_top:
+		tiles_in_this_wall.reverse()
+		return tiles_in_this_wall
+	else:
+		return tiles_in_this_wall
 	
 func build_vertical_wall(
 	wall_length: int,
@@ -76,11 +91,20 @@ func build_vertical_wall(
 	x: int,  # x-coord of wall
 	y: int,  # Starting y-coord of wall
 	second_level_offset: int, 
-	x_y_offset: Vector2i
-):
+	x_y_offset: Vector2i,
+	left_not_right: bool, # true == left, false == right
+) -> Array[Node]:
+	var tiles_in_this_wall: Array[Node] = []
+	
+	var level_offsets = [Vector2i.ZERO, Vector2i(0, second_level_offset)]
+	if not left_not_right:
+		level_offsets.reverse()
+	
 	for i in wall_length:
-		for level_offset in [Vector2i.ZERO, Vector2i(0, second_level_offset)]:
+		for level_offset in level_offsets:
 			var tile = tiles.pop_front()
+			tiles_in_this_wall.append(tile)
+			
 			tile.rest_point = Vector2i(x, y) + x_y_offset*i + level_offset
 			
 			# HACK: These get built top-to-bottom, so *every* time a new one gets placed,
@@ -89,30 +113,36 @@ func build_vertical_wall(
 			tile.frozen = false
 			tile.perspective = Common.TilePerspective.LEFT
 			
-			await get_tree().create_timer(0.05).timeout
+			
+			await get_tree().create_timer(0.01).timeout
+	
+	if left_not_right:
+		tiles_in_this_wall.reverse()
+		return tiles_in_this_wall
+	else:
+		return tiles_in_this_wall
 
 
-func build_wall():
+func build_wall() -> Array[Node]:
 	# TODO I want this to be done when the animations finish... needs to wait on
 	# an event or something signalling that each tile is at rest.
 	
 	get_tree().call_group("tiles", "set_faceup", false)
 	var tiles = get_tree().get_nodes_in_group("tiles")
 	tiles.shuffle()
-	var ret = tiles.duplicate()  # TODO ugh... please for the love of god, no.
 	
 	# Right now we're hard-coded to 1600x900, so assume those values when making adjustments.
 	# TODO this will certainly bite me in the ass later. Oh well :shrug:
 	# TODO Tiles is not consumed after these... I assume it's an array of refs so 
 	# it's probably cheap to copy into these functions
 	# UH MAYBE NOT ACTUALLY
-	build_horizontal_wall(Common.wall_length, tiles, 325,      900-140, -20, Vector2i(52, 0)) # lower
-	build_vertical_wall(Common.wall_length, tiles,   325-21*3,      50, -20, Vector2i(0, 52-12)) # left
-	build_horizontal_wall(Common.wall_length, tiles, 325,      60, -20, Vector2i(52, 0)) # upper
-	build_vertical_wall(Common.wall_length, tiles,   1600-325+17*3, 50, -20, Vector2i(0, 52-12)) # right
+	var bottom_idxs = await build_horizontal_wall(Common.wall_length, tiles, 325, 900-140, -20, Vector2i(52, 0), true) # lower
+	var left_idxs = await build_vertical_wall(Common.wall_length, tiles,   325-21*3, 50, -20, Vector2i(0, 52-12), true) # left
+	var top_idxs = await build_horizontal_wall(Common.wall_length, tiles, 325, 60, -20, Vector2i(52, 0), false) # upper
+	var right_idxs = await build_vertical_wall(Common.wall_length, tiles,   1600-325+17*3, 50, -20, Vector2i(0, 52-12), false) # right
 	
 	# We need the new ordering of the tiles in further steps. Return it here
-	return ret
+	return bottom_idxs + left_idxs + top_idxs + right_idxs
 
 func create_hands():
 	const hand_scene = preload("res://hand.tscn")
@@ -155,7 +185,7 @@ func _on_new_game():
 	var tiles_wall_order = await build_wall()
 	# TODO this is just a bandaid solution... build_wall should probably be 
 	# awaitable and have some sort of signal that pops when it's done building.
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(0.5).timeout
 	create_hands()
 	var first_col_offset = await spawn_dice()
 	deal(tiles_wall_order, first_col_offset)
@@ -167,7 +197,7 @@ func deal(tiles_wall_order: Array, first_col_offset: int):
 	#var tiles: Array = get_tree().get_nodes_in_group("tiles")
 	for tile in tiles_wall_order:
 		tile.faceup = true
-		await get_tree().create_timer(0.25).timeout
+		await get_tree().create_timer(0.2).timeout
 		#tile.faceup = false
 		#await get_tree().create_timer(0.5).timeout
 		
